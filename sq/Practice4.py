cb30d67d01538ccc9b1f197873675cf9076be2fd
siqi <leisiqi2006@gmail.com>
2018-11-11 16:29:22 +0800

    yay

--- sq/Practice4.py
+++ sq/Practice4.py
@@ -3,3 +3,6 @@ n = math.factorial(5)
 k = math.factorial(3)
 a = math.factorial(5-3)
 print(n/(k*a))
+
+"""#There are 5 different types of fruits, take three from it each time, calculate how many 
+different takes can be done - hint: n!/k!(n-k)!"""
