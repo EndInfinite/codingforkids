b6ec4fbb91e6b0c8786c05937e645c51eb1a7a43
siqi <leisiqi2006@gmail.com>
2018-11-24 18:02:21 +0800

    yay

--- sq/Practice8.py
+++ sq/Practice8.py
@@ -1,11 +1,12 @@
 a =[1,1,3,5,9,20,34,55,73,98]
-b = [1,2,3,4,7,9,34,35.38]
+b = [1,2,3,4,7,9,34,35,38]
 
-myList = []
+def myNumbers(a,b):
+    myList = []
+    for item in b:
+        if item in a:
+            myList.append(item)
+    return myList
 
-for item in b:
-    if item in a:
-        myList.append(item)
-return myList
-
-print(myList)
+myAnswer = myNumbers(a,b)
+print(myAnswer)
