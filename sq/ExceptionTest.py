f4fbfde258100710cf91f4051b2b6e2e5b937f82
siqi <leisiqi2006@gmail.com>
2018-12-02 16:04:28 +0800

    yay

--- sq/ExceptionTest.py
+++ sq/ExceptionTest.py
@@ -12,3 +12,37 @@ def convertToInt2(s):
         print("conversion failed, unable to convert '{0}' to an integer.".format(s))
         return -1
 
+def convertToInt3(s):
+    try:
+        x = int(s)
+        print("conversion is successful, the converted value is:", x)
+        return x
+    except ValueError:
+        print("conversion failed due to ValueError, unable to convert '{0}' to an integer.".format(s))
+        return -1
+    except TypeError:
+        print("conversion failed due to TypeError, unable to convert '{0}' to an integer.".format(s))
+        return -1
+
+def convertToInt4(s):
+    try:
+        x = int(s)
+        print("conversion is successful, the converted value is:", x)
+        return x
+    except ValueError as e:
+        print("conversion failed due to ValueError, unable to convert '{0}' to an integer. The exception message is: {1}".format(s, str(e)))
+        return -1
+    except TypeError as e:
+        print("conversion failed due to TypeError, unable to convert '{0}' to an integer. The exception message is: {1}".format(s, str(e)))
+        return -1
+
+def convertToInt5(s):
+    try:
+        x = int(s)
+        print("conversion is successful, the converted value is:", x)
+        return x
+    except Exception as e:
+        print("conversion failed due to ValueError, unable to convert '{0}' to an integer. The exception message is: {1}".format(s, str(e)))
+        return -1
+   
+
