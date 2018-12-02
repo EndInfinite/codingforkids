a0925e6900e4c62701958a56153da2b4c31bdaea
siqi <leisiqi2006@gmail.com>
2018-12-02 16:30:18 +0800

    .

--- sq/OpenFileTest.py
+++ sq/OpenFileTest.py
@@ -1,7 +1,7 @@
 import sys
 
 def writeToFile():
-    f = open("myfile.txt", mode="wt", encoding="ascii") #"utf-8"
+    f = open("myfile.txt", mode="wt", encoding="utf-8") #don't use ascii
     f.write("my name is Nobody.")
     f.write("I am 12.\n")
     f.write("Who are you?\n")
@@ -9,4 +9,13 @@ def writeToFile():
     f.write("o(*￣▽￣*)ブ\n")
     f.close()
 
+
+
+def writeToFile2():
+    f = open("myfile.txt", mode="wt", encoding="utf-8")
+    f.write("My PSLE result is XXX\n")
+    f.write("我很伤心。\n")
+    f.close()
+
 writeToFile()
+writeToFile2()
