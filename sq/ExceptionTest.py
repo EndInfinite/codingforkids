e9caabcbe5af29d90282aca3124c3684452c65a3
siqi <leisiqi2006@gmail.com>
2018-12-02 15:18:17 +0800

    yay

--- sq/ExceptionTest.py
+++ sq/ExceptionTest.py
@@ -3,3 +3,12 @@ def convertToInt(s):
     print("conversion is successful, the converted value is:", x)
     return x
 
+def convertToInt2(s):
+    try:
+        x = int(s)
+        print("conversion is successful, the converted value is:", x)
+        return x
+    except ValueError
+        print("conversion failed, unable to convert '{0}' to an integer.".format(s))
+        return -1
+
