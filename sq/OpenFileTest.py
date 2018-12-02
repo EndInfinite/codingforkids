cfa88008b19ea16543930fa557f32e77ecfdeed8
siqi <leisiqi2006@gmail.com>
2018-12-02 16:34:18 +0800

    .

--- sq/OpenFileTest.py
+++ sq/OpenFileTest.py
@@ -9,13 +9,17 @@ def writeToFile():
     f.write("o(*￣▽￣*)ブ\n")
     f.close()
 
-
-
 def writeToFile2():
     f = open("myfile.txt", mode="wt", encoding="utf-8")
     f.write("My PSLE result is XXX\n")
     f.write("我很伤心。\n")
     f.close()
 
+def writeToFile3():
+    f = open("myfile.txt", mode="at", encoding="utf-8")
+    f.write("My PSLE result is XXX\n")
+    f.write("我很伤心。\n")
+    f.close()
+
 writeToFile()
-writeToFile2()
+writeToFile3()
